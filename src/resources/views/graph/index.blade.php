27739cad7a497fdf4e25ea7e14bef6c96bdc5b69
achmadgani <a.gani@iforcom.jp>
2020-11-09 10:40:27 +0900

    edited shellexec instead of using symphony

--- src/resources/views/graph/index.blade.php
+++ src/resources/views/graph/index.blade.php
@@ -1,49 +1,28 @@
-<?
-use Symfony\Component\Process\Process;
-use Symfony\Component\Process\Exception\ProcessFailedException;
-
-$text = 'shimizu.csv';
-$process = new Process(['python3',  '--version']);
-//$process = new Process(["python /Path/To/analyse_string.py \"{$text}\""]);
-$process->run();
-
-// executes after the command finishes
-if (!$process->isSuccessful()) {
-    throw new ProcessFailedException($process);
-}
-
-if(isset($_POST['button1'])) { 
-            echo 'tst';
-        } 
-
-// Result (string): {'neg': 0.204, 'neu': 0.531, 'pos': 0.265, 'compound': 0.1779}
-?>
-
 <head>
 	<meta charset="UTF-8">
 	<!-- Scripts -->
 	<script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 	<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"></script>
-   
+
     <!-- Styles -->
 	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.css">
 <head>
-
-<div class="container" align="center">
-	<h1>一覧画面</h1>
-	
-</div>
-    <form method="post"> 
-        <input type="submit" name="button1"
-                value="Button1"/> 
-    </form> 
 <div class="card">
 	<div class = "card-header">
 	</div>
-	
+
 	<div class= "card-body">
-		<h3 class="mb-0" align="center">Prediction Value</h5>
+		</h5>
+        <h3 class="mb-0" align="center">Prediction Value</h5>
+        <div align="center">
+            <?php
+            // Use ls command to shell_exec function
+            // $output = shell_exec('python3 py_graph/add_graph.py');
+            $output = shell_exec('python3 --version');
+            echo "<pre>$output</pre>";
+            ?>
+        </div>
 		<div class = "row">
 			<div class="col-xs-12 col-md-12 text-left">
 			 <!-- chart start -->
